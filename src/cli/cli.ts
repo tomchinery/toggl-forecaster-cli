c390298ed97f76d8a36e9c7832a996f3cd373379
Tom Chinery <tom.chinery@addtoevent.co.uk>
2020-05-22 07:21:13 +0100

    chore(init): initialise typescript cli

---
+++ src/cli/cli.ts
@@ -0,0 +1,8 @@
+// import { main } from '../main/main';
+
+interface CLIArgs {}
+
+export async function cli(args: CLIArgs): Promise<void> {
+    // call another function here
+    console.log('hello!');
+}
